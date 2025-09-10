5a7ae32c096283adde6c923999d758ca3d0618b3
Gabriel <gabriel.lopes176@etec.sp.gov.br>
2025-09-10 00:39:13 -0300

    Criado a Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="w-full bg-[#18396F] shadow-md fixed top-0 left-0 z-50">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-2xl font-bold text-white">MinhaLogo</div>
+
+        {/* Links  */}
+        <div className="hidden md:flex items-center space-x-8">
+          <a href="#" className="text-white font-semibold hover:text-blue-600">
+            Produtos
+          </a>
+          <a href="#" className="text-white font-semibold hover:text-blue-600">
+            Planos
+          </a>
+          <a href="#" className="text-white font-semibold hover:text-blue-600">
+            Soluções
+          </a>
+          <a href="#" className="text-white font-semibold hover:text-blue-600">
+            Sobre Nós
+          </a>
+          <button className="px-6 py-1 rounded-3xl bg-white text-black font-semibold hover:bg-blue-700 transition">
+            Entrar
+          </button>
+        </div>
+
+        {/* Botão menu mobile */}
+        <button
+          className="md:hidden text-white"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+      </div>
+
+      {/* Menu mobile */}
+      {menuOpen && (
+        <div className="md:hidden bg-white shadow-md px-6 py-4 flex flex-col space-y-4">
+          <a href="#" className="text-gray-700 hover:text-blue-600">
+            Produtos
+          </a>
+          <a href="#" className="text-gray-700 hover:text-blue-600">
+            Planos
+          </a>
+          <a href="#" className="text-gray-700 hover:text-blue-600">
+            Soluções
+          </a>
+          <a href="#" className="text-gray-700 hover:text-blue-600">
+            Sobre Nós
+          </a>
+          <button className="px-4 py-2 rounded-xl bg-blue-600 text-white hover:bg-blue-700 transition">
+            Entrar
+          </button>
+        </div>
+      )}
+    </nav>
+  );
+}
+
