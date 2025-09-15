ac0915374e29e90d2edf2e9279fa605829826826
Gabriel371-tech <gabriel.lopes176@etec.sp.gov.br>
2025-09-15 20:10:26 -0300

    modificado Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,21 +3,22 @@ import estudanteFlutuando from '../assets/estudante_flutuando_longe.gif';
 
 const Hero: React.FC = () => {
   return (
-    <section className="w-full min-h-screen flex flex-col md:flex-row items-center justify-between px-8 md:px-16 lg:px-24 py-12 bg-[#18396F]">
+    <section className="w-full min-h-screen flex flex-col 2xl:px-40 md:flex-row items-center justify-between px-8 md:px-16 lg:px-24 py-12 bg-[#18396F]">
       {/* Lado Esquerdo - Textos */}
       <div className="flex-1 text-center md:text-left">
-        <h1 className="font-poppins font-semibold text-3xl md:text-4xl lg:text-5xl text-white leading-snug">
+        <h1 className="font-poppins font-semibold text-3xl 2xl:text-6xl md:text-4xl lg:text-5xl text-white leading-snug">
           Sistema com controle e <br />
           eficiência para <br />
           instituição acadêmica
         </h1>
 
-        <p className="font-poppins text-white mt-6 text-lg md:text-xl max-w-lg">
+        <p className="font-poppins text-white mt-6 text-lg 2xl:text-5x1 md:text-xl max-w-lg">
           Conectando alunos, professores e a administração para um aprendizado
           de excelência. O futuro da gestão educacional, construído em
           colaboração.
         </p>
-
+        
+        {/* Botao */}
         <button className="mt-8 bg-white text-black font-poppins font-medium px-8 py-3 rounded-3xl shadow-md hover:scale-105 transition-transform">
           Começe agora
         </button>
@@ -28,7 +29,7 @@ const Hero: React.FC = () => {
         <img
         src={estudanteFlutuando} 
         alt="Gestão acadêmica animada"
-        className="w-72 md:w-80 lg:w-96 object-contain"
+        className="w-72 2xl:w-100 md:w-80 lg:w-96 object-contain"
         />
       </div>
     </section>
