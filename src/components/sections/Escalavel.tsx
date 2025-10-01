e9beb49d3b2e5a915c7a9b5e09f232e8c0b1b208
Gabriel371-tech <gabriel.lopes176@etec.sp.gov.br>
2025-10-01 20:19:21 -0300

    modificado os components Icones e Escalavel

--- src/components/sections/Escalavel.tsx
+++ src/components/sections/Escalavel.tsx
@@ -5,11 +5,11 @@ import building from '../../assets/icons/mdi--building.png';
 export default function Escalavel () {
     return(
 
-        <section className="w-full h-screen bg-zinc-50 py-12 px-4 flex flex-col items-center justify-center">
+        <section className="w-full min-h-screen bg-zinc-50 py-12 px-4 flex flex-col items-center justify-center">
             <h1 className="text-black text-center font-poppins font-semibold text-4xl">Totalmente</h1>
             <h2 className="text-[#5975FF] font-poppins text-center text-6xl mb-6 underline">Escalável!</h2>
             <h3 className="text-black text-center text-4xl font-poppins font-light mt-4 mb-8">Desenvolvido para suportar qualquer tipo de infraestrutura!</h3>
-            <div className="grid grid-cols-3 gap-y-8 gap-x-[180px] mt-8">
+            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-y-8 gap-x-[180px]">
                 <div className="flex flex-col items-center">
                     <img src={usergroup} alt="universidade" />
                     <span className='mt-6 text-black text-center text-3xl font-medium break-normal'>Pequenas<br/> Empresas</span>
--- src/components/sections/Icones.tsx
+++ src/components/sections/Icones.tsx
@@ -1,6 +1,10 @@
-import colegio from '../../assets/icons/colegio.png';
+import colegio from '../../assets/icons/teenyicons--school-outline.png';
 import ensino from '../../assets/icons/ensino.png';
 import universidade from '../../assets/icons/universidade.png';
+import hat from '../../assets/icons/icon-park-outline--degree-hat.png';
+import organization from '../../assets/icons/streamline-ultimate--content-paper-edit-bold.png';
+import search from '../../assets/icons/material-symbols--search.png';
+
 export default function Icones() {
     return (
 
@@ -17,31 +21,31 @@ export default function Icones() {
                 {/* ICONE 2*/}
                 <div className="flex flex-col items-center">
                     <img src={colegio} alt="" className="w-61 h-61 " />
-                    <span className="mt-2 text-center text-black font-semibold">Universidades</span>
+                    <span className="mt-2 text-center text-black font-semibold">Colegio</span>
                 </div>
 
                 {/* ICONE 3*/}
                 <div className="flex flex-col items-center">
                     <img src={ensino} alt="" className="w-61 h-61 " />
-                    <span className="mt-2 text-center text-black font-semibold">Universidades</span>
+                    <span className="mt-2 text-center text-black font-semibold">Ensino</span>
                 </div>
 
                 {/* ICONE 4*/}
                 <div className="flex flex-col items-center">
-                    <img src={universidade} alt="" className="w-61 h-61 " />
-                    <span className="mt-2 text-center text-black font-semibold">Universidades</span>
+                    <img src={organization} alt="" className="w-61 h-61 " />
+                    <span className="mt-2 text-center text-black font-semibold">Organizações</span>
                 </div>
 
                 {/* ICONE 5*/}
                 <div className="flex flex-col items-center">
-                    <img src={universidade} alt="" className="w-61 h-61 " />
-                    <span className="mt-2 text-center text-black font-semibold">Universidades</span>
+                    <img src={hat} alt="" className="w-61 h-61 " />
+                    <span className="mt-2 text-center text-black font-semibold">Academicos</span>
                 </div>
 
                 {/* ICONE 6*/}
                 <div className="flex flex-col items-center">
-                    <img src={universidade} alt="" className="w-61 h-61 " />
-                    <span className="mt-2 text-center text-black font-semibold">Universidades</span>
+                    <img src={search} alt="" className="w-61 h-61 " />
+                    <span className="mt-2 text-center text-black font-semibold">Pesquisa</span>
                 </div>
             </div>
         </section>
